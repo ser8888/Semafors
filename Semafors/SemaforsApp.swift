1c03a4daa491656715277d6e9e37bfb4df621ded
Sergejs Tiselskis <slokas85@gmail.con>
2022-09-10 06:24:38 +0300

    Initial Commit

---
+++ Semafors/SemaforsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SemaforsApp.swift
+//  Semafors
+//
+//  Created by John Doe on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SemaforsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
